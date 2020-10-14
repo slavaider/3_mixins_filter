b7e6e640593363fe67f636593d53923c2e1be43c
slavaider <slavaider1@gmail.com>
2020-10-14 21:04:02 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,106 @@
+/* eslint-disable no-console */
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h2>{{Title}}</h2>
+        <label>
+            <input type="text" v-model="searchName"/>
+        </label>
+        <ul>
+            <li v-for="name of filterNames" :key="name">{{ name }}</li>
+        </ul>
+        <hr/>
+        <app-counter></app-counter>
+        <app-car>
+            <h2 slot="title">{{carName}}</h2>
+            <p slot="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id quibusdam incidunt magni
+                expedita tenetur esse aliquid officia impedit assumenda amet eos, nulla ut ratione nostrum nobis aut
+                accusamus, vel eius?</p>
+        </app-car>
+        <hr/>
+        <h2>Form</h2>
+        <label>
+            <input type="text" v-model.lazy="name"/>
+        </label>
+        <p>{{name}}</p>
+        <hr/>
+        <label>
+            <textarea v-model="textarea"></textarea>
+        </label>
+        <p>{{textarea}}</p>
+        <hr/>
+        <label for="instagram">INSTAGRAM</label>
+        <input type="radio" id="instagram" value="instagram" v-model="social"/>
+        <label for="vk">VK</label>
+        <input type="radio" id="vk" value="vk" v-model="social"/>
+        <ul>{{social}}</ul>
+        <hr/>
+        <label>
+            <select v-model="def">
+                <option v-for="s of select" :key="s">{{s}}</option>
+            </select>
+        </label>
+        <hr/>
+        <label>
+            <input type="number" v-model.number="age"/>
+        </label>
+        <p>{{age}}</p>
+        <hr/>
+        <button class="on">on</button>
+        <button class="off">off</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import listMixin from "./listMixin";
+    import car from "./components/car/car"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "app",
+        components: {
+            "app-car": car
+        },
+        mixins: [listMixin],
+        watch: {
+            age(value) {
+                // eslint-disable-next-line no-console
+                console.log(value);
+            }
+        },
+        data: function () {
+            return {
+                age: 20,
+                carName: "Ford",
+                Title: "Title",
+                visible: true,
+                name: "input",
+                textarea: "lol",
+                social: "",
+                select: ["lol", "lol2"],
+                def: "lol2"
+            };
+        },
+        directives: {
+            font: {
+                bind(el) {
+                    el.style.fontSize = "100px";
+                }
+            }
+        },
+        filters: {
+            lowercase(el) {
+                return el.toLowerCase();
+            }
+        }
+    };
 </script>
+<style scoped>
+    textarea {
+        height: 100px;
+        width: 500px;
+    }
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    p {
+        white-space: pre;
+    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ColorDirective from './components/color'
+import Counter from "./components/counter/counter.vue";
 
 Vue.config.productionTip = false
-
+Vue.filter('uppercase', value => value.toUpperCase())
+Vue.directive('ColorDirective', ColorDirective)
+Vue.component('app-counter', Counter)
+export const eventEmitter = new Vue();
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App),
+});
+
